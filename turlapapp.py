5b001a25b6b4ed10e0ae8998a73765ac2201824b
gentambob <44304798+gentambob@users.noreply.github.com>
2024-02-21 17:56:20 +0900

    last

--- turlapapp.py
+++ turlapapp.py
@@ -9,7 +9,7 @@ import streamlit as st
 from streamlit_folium import folium_static
 import pandas as pd
 import geopandas as gpd
-#import shapely 
+import shapely 
 #from osmnx.projection import project_gdf
 from streamlit_geolocation import streamlit_geolocation
 #import pyperclip
@@ -19,26 +19,47 @@ st.set_page_config(layout="wide")
 
 
 
-file2="/Users/genta/Desktop/turlap/hayashidata_within.gpkg"
-file1="/Users/genta/Desktop/turlap/grid_data_forqgis.gpkg"
-file3="/Users/genta/Desktop/turlap/mamikos.geojson"
-
+file1="grid_data_forqgis.gpkg"
+file2="hayashidata_within.gpkg"
+kebo="kebo.gpkg"
+notkebo="notkebo.gpkg"
 #f=gpd.read_file(file2)
 #f.plot("LocalMoranHotCold:All RSBs")
 
 options_list=["hotspot portal (kampung)", "hotspot portal (komplek)",
-            "coldspot portal(kampung)", "coldspot portal(komplek)", "kosan"]
+            "coldspot portal(kampung)", "coldspot portal(komplek)", "kosan (gak boleh kumpul kebo)", "kosan (gak larang kumpul kebo)"]
 @st.cache(suppress_st_warning=True,allow_output_mutation=True) 
 def data():
     gdf=gpd.read_file(file1).to_crs("epsg:4326")
     hayashi=gpd.read_file(file2).to_crs("epsg:4326")
-    kebo=gpd.read_file(file3).to_crs("epsg:4326")
+    
+
     dictio={}
     dictio["hotspot portal (kampung)"]=gpd.sjoin(hayashi[hayashi["BE_2016"]=="カンポン"], gdf[gdf["LocalMoranHotCold:All RSBs"]=="HH"])
     dictio["hotspot portal (komplek)"]=gpd.sjoin(hayashi[hayashi["BE_2016"]=="計画"], gdf[gdf["LocalMoranHotCold:All RSBs"]=="HH"])
     dictio["coldspot portal (komplek)"]=gpd.sjoin(hayashi[hayashi["BE_2016"]=="計画"], gdf[gdf["LocalMoranHotCold:All RSBs"]=="LL"])
     dictio["coldspot portal (kampung)"]=gpd.sjoin(hayashi[hayashi["BE_2016"]=="カンポン"], gdf[gdf["LocalMoranHotCold:All RSBs"]=="LL"])
-    dictio["kosan"]=kebo
+    """
+    file="/Volumes/HDPH-UT/DATA/scraping/ruko_indonesia/2022-07-09_mamikos.csv"
+    gdf=gpd.GeoDataFrame(pd.read_csv(file), geometry=pd.read_csv(file)["geometry"].apply(shapely.wkt.loads),
+                    crs= "EPSG:3395")
+
+    cols=gdf.dtypes.rename("type")
+    for i, c in zip(cols.index,cols):
+        if c.name=='bool':
+            gdf["dummy"+str(i)]=gdf[i].replace({True:1, False:0})
+
+    gdf["dummymarried_couple_allowed"]=gdf["dummymarried_couple_allowed"].replace({0:1, 1:0})
+    kebo=gdf[gdf[["dummyopposite_sex_not_allowed", "dummygendered","dummymarried_couple_allowed"]].sum(1)>0]
+    notkebo=gdf.loc[gdf.index.isin(kebo.index)==False]
+    kebo.to_crs("epsg:4326")[["geometry", "name_slug"]].to_file("kebo.gpkg",  driver="GPKG")
+    notkebo.to_crs("epsg:4326")[["geometry", "name_slug"]].to_file("notkebo.gpkg",  driver="GPKG")
+    #cctv=gdf[gdf["dummyCCTV"]>0]
+    #guard=gdf[gdf[ 'dummyguard']>0]
+ 
+    """
+    dictio["kosan (gak boleh kumpul kebo)"]=gpd.read_file(kebo)
+    dictio["kosan (gak larang kumpul kebo)"]=gpd.read_file(notkebo)
     return dictio
 dictio=data()
 
